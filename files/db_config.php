c6d7c153a68729b6f14e04738331347c19b8993a
Aaghash R <aaghash.r@in.abb.com>
2023-11-04 19:49:09 +0530

    hello

--- files/db_config.php
+++ files/db_config.php
@@ -1,18 +1,22 @@
 <?php
 
 // Free My SQL DB
-
-$servername = "sql12.freesqldatabase.com";
-$username = "sql12657337";
-$password = "xDESGG7pQ3";
-$dbname = "sql12657337";
+//$servername = "sql12.freesqldatabase.com";
+//$username = "sql12657337";
+//$password = "xDESGG7pQ3";
+//$dbname = "sql12657337";
 
 // Webhost DB
-
 //$servername = "localhost";
 //$username = "id21476807_archery123";
 //$password = "P@ssword@2023";
 //$dbname = "id21476807_archery";
+
+//Local Connection
+$servername = "localhost";
+$username = "root";
+$password = "admin123";
+$dbname = "archery";
 
 // Create a connection
 $conn = new mysqli($servername, $username, $password, $dbname);
