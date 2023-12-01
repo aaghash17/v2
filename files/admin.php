e0d09935e09db266de21ebe030a7ee7820adca51
Aaghash R <aaghash.r@in.abb.com>
2023-12-01 15:43:19 +0530

    hai

--- files/admin.php
+++ files/admin.php
@@ -1,57 +1,6 @@
 <?php
 require_once 'db_config.php';
 
-$result1 = $conn->query("SELECT * FROM participant");
-$user_arr[] = array('tboard', 'tplayer', 'pname', 'district', 'category', 'total', 'd11', 'd12', 'd13', 'd21', 'd22', 'd23', 'd31', 'd32', 'd33', 'd41', 'd42', 'd43', 'd51', 'd52', 'd53', 'd61', 'd62', 'd63', 'd71', 'd72', 'd73', 'd81', 'd82', 'd83', 'd91', 'd92', 'd93', 'd101', 'd102', 'd103');
-while ($row = mysqli_fetch_array($result1)) {
-  $a1 = $row['tboard'];
-  $a2 = $row['tplayer'];
-  $a3 = $row['pname'];
-  $a4 = $row['district'];
-  $a5 = $row['category'];
-  $a8 = $row['total'];
-  $a9 = $row['d11'];
-  $a10 = $row['d12'];
-  $a11 = $row['d13'];
-  $a12 = $row['d21'];
-  $a13 = $row['d22'];
-  $a14 = $row['d23'];
-  $a15 = $row['d31'];
-  $a16 = $row['d32'];
-  $a17 = $row['d33'];
-  $a18 = $row['d41'];
-  $a19 = $row['d42'];
-  $a20 = $row['d43'];
-  $a21 = $row['d51'];
-  $a22 = $row['d52'];
-  $a23 = $row['d53'];
-  $a24 = $row['d61'];
-  $a25 = $row['d62'];
-  $a26 = $row['d63'];
-  $a27 = $row['d71'];
-  $a28 = $row['d72'];
-  $a29 = $row['d73'];
-  $a30 = $row['d81'];
-  $a31 = $row['d82'];
-  $a32 = $row['d83'];
-  $a33 = $row['d91'];
-  $a34 = $row['d92'];
-  $a35 = $row['d93'];
-  $a36 = $row['d101'];
-  $a37 = $row['d102'];
-  $a38 = $row['d103'];
-  $user_arr[] = array(
-    $a1, $a2, $a3, $a4, $a5, $a8, $a9, $a10,
-    $a11, $a12, $a13, $a14, $a15, $a16, $a17, $a18, $a19, $a20,
-    $a21, $a22, $a23, $a24, $a25, $a26, $a27, $a28, $a29, $a30,
-    $a31, $a32, $a33, $a34, $a35, $a36, $a37, $a38
-  );
-}
-$serialize_user_arr = serialize($user_arr);
-
-?>
-
-<?php
 session_start();
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   //if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
@@ -205,97 +154,100 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       <h4>Export Data</h4>
       <form method='post' action='download.php'>
         <button type='submit' class='btn btn-primary'>Export</button>
-        <textarea name='export_data' style='display: none;'><?php echo $serialize_user_arr; ?></textarea>
+        <textarea name='export_data' style='display: none;'><?php require_once 'export_data.php' ?></textarea>
       </form>
+      </div-->
+
+      <div class="container mt-2">
+        <h4>Clear Table Data</h4>
+        <button type="button" class="btn btn-danger" onclick="clearTable()">Clear Table</button>
+      </div>
+
+      <?php
+      include "user-index.php";
+      ?>
+
+
+      <?php
+      $result = $conn->query("SHOW COLUMNS FROM participant FROM " . $dbname);
+      while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+      }
+      $columnArr = array_column($data, 'Field');
+      $result = $conn->query("SELECT * FROM participant");
+      ?>
+
+      <div class="table-container">
+        <table class="table table-bordered">
+          <thead class="table-dark sticky-header">
+            <tr>
+
+              <?php
+              foreach ($columnArr as $value) {
+                echo "<th>" . $value . "</th>";
+              }
+              ?>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+            while ($row = mysqli_fetch_assoc($result)) {
+              echo "<tr>";
+              foreach ($columnArr as $a) {
+                echo "<td>" . $row[$a] . "</td>";
+              }
+              echo "</tr>";
+            }
+            ?>
+          </tbody>
+        </table>
+      </div>
     </div>
 
-    <div class="container mt-2">
-      <h4>Clear Table Data</h4>
-      <button type="button" class="btn btn-danger" onclick="clearTable()">Clear Table</button>
-    </div>
-
-    <?php
-    include "user-index.php";
-    ?>
-
-  </div>
-
-  <?php
-  $result = $conn->query("SHOW COLUMNS FROM participant FROM " . $dbname);
-  while ($row = $result->fetch_assoc()) {
-    $data[] = $row;
-  }
-  $columnArr = array_column($data, 'Field');
-  $result = $conn->query("SELECT * FROM participant");
-  ?>
-
-  <div class="table-container">
-    <table class="table table-bordered">
-      <thead class="table-dark sticky-header">
-        <tr>
-
-          <?php
-          foreach ($columnArr as $value) {
-            echo "<th>" . $value . "</th>";
+    <script>
+      function setSessionVariable(data) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', '', true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            if (data[0] == 1) {
+              result = xhr.responseText;
+              document.getElementById("event-name").value = result;
+            }
+            if (data[0] == 2) {
+              result = xhr.responseText;
+              alert("Event name changed");
+              location.reload();
+            }
+            if (data[0] == 3) {
+              result = xhr.responseText;
+              alert("Table cleared");
+              location.reload();
+            }
           }
-          ?>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-        while ($row = mysqli_fetch_assoc($result)) {
-          echo "<tr>";
-          foreach ($columnArr as $a) {
-            echo "<td>" . $row[$a] . "</td>";
-          }
-          echo "</tr>";
-        }
-        ?>
-      </tbody>
-    </table>
-  </div>
+        };
+        xhr.send('data=' + encodeURIComponent(data));
+      }
 
+      function loadfunc() {
+        setSessionVariable("1");
+      }
 
-  <script>
-    function setSessionVariable(data) {
-      var xhr = new XMLHttpRequest();
-      xhr.open('POST', '', true);
-      xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-      xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-          if (data[0] == 1) {
-            result = xhr.responseText;
-            document.getElementById("event-name").value = result;
-          }
-          if (data[0] == 2) {
-            result = xhr.responseText;
-            alert("Event name changed");
-            location.reload();
-          }
-          if (data[0] == 3) {
-            result = xhr.responseText;
-            alert("Table cleared");
-            location.reload();
-          }
-        }
-      };
-      xhr.send('data=' + encodeURIComponent(data));
-    }
+      function myEvent() {
+        setSessionVariable("2" + document.getElementById("event-name").value);
+      }
 
-    function loadfunc() {
-      setSessionVariable("1");
-    }
+      function clearTable() {
+        if (confirm("Do you want to clear Table!")) {
+          setSessionVariable("3");
+        } else {}
+      }
 
-    function myEvent() {
-      setSessionVariable("2" + document.getElementById("event-name").value);
-    }
-
-    function clearTable() {
-      if (confirm("Do you want to clear Table!")) {
-        setSessionVariable("3");
-      } else {}
-    }
-  </script>
+      function updateExportData() {
+        setSessionVariable("4");
+      }
+    </script>
 
 </body>
 
