8e3edef8c41aecca34c25d0937f2d05020dd5913
Aaghash R <aaghash.r@in.abb.com>
2023-12-01 11:58:43 +0530

    new

--- files/user-add.php
+++ files/user-add.php
@@ -44,12 +44,8 @@ if (isset($_POST["submit"])) {
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link rel="icon" type="image/x-icon" href="images/title.ico">
 
-   <!-- Bootstrap -->
-   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-
-   <!-- Font Awesome -->
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
+   <link href="bootstrap-5.2.3-dist/css/bootstrap.min.css" rel="stylesheet">
+   <script src="bootstrap-5.2.3-dist/js/bootstrap.bundle.min.js"></script>
    <title>Add User</title>
 </head>
 
@@ -90,10 +86,6 @@ if (isset($_POST["submit"])) {
          </form>
       </div>
    </div>
-
-   <!-- Bootstrap -->
-   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-
 </body>
 
 </html>
