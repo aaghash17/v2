be1a5b77d010e58063ec482d61838e38e3d93890
Aaghash R <aaghash.r@in.abb.com>
2023-12-01 10:31:17 +0530

    new

--- files/login.php
+++ files/login.php
@@ -37,8 +37,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <title>Login</title>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet" />
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!--link rel="stylesheet" href="css/4.7.0/font-awesome.min.css"-->
   <link rel="icon" type="image/x-icon" href="images/title.ico">
   <link rel="stylesheet" href="css/style.css" />
 </head>
